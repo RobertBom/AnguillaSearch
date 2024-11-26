79618395dd6fef5362e9a05f1d3dccf9dae92096
Robert Bombka <r.bombka@gmail.com>
2024-11-26 18:48:59 +0100

    Added tokLem function to Parser Class
    The tokLem function to tokenize and lemmatize Strings has been added to
    the Parser class. A function for stop word removal is called, but still
    has to be implemented.

--- src/main/java/de/fernunihagen/dbis/anguillasearch/Parser.java
+++ src/main/java/de/fernunihagen/dbis/anguillasearch/Parser.java
@@ -1,10 +1,16 @@
 package de.fernunihagen.dbis.anguillasearch;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 
 /**
  * Parser Class which provides several functions.
@@ -30,5 +36,50 @@ public class Parser {
         }
 
         return new Page(url, title, header, content, linkList);
-    }       
+    }
+
+    /**
+     * 
+     * 
+    */
+    public static String rmStopWords(final String inputString) {
+        /** the String we work on */
+        String workString = inputString;
+        return workString;
+    }
+
+    /**
+     * Tokenizes the input String.
+     * e.g. 
+     * @param content   The String which should be tokenized
+     * @return  an ArrayList<String> of the token of content.
+     */
+    public static ArrayList<String> tokenize(final String inputString) {
+        ArrayList<String> outputList = new ArrayList<>();
+        
+        /** the String which we manipulate to get the tokenized and lemmatized result */
+        String workString = inputString;
+
+        //remove StopWords
+        workString = rmStopWords(workString);
+        //remove punctation marks, 
+        workString = workString.replaceAll("[\\.!,]", "");
+
+        //create out StanfordCoreNLP Pipeline with the right annotators
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        //process our workstring
+        CoreDocument coreDoc = new CoreDocument(workString);
+        pipeline.annotate(coreDoc);
+
+
+        List<CoreLabel> tokenList = coreDoc.tokens();
+        for(CoreLabel token : tokenList) {
+            outputList.add(token.lemma());
+        }
+
+        return outputList;
+    }
 }
