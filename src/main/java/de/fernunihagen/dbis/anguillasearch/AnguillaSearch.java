a3a7029e21006f59c78d646faa02ab9aea7cc3ac
Robert Bombka <r.bombka@gmail.com>
2024-12-11 21:50:36 +0100

    Fixed match highlighting changing case

--- src/main/java/de/fernunihagen/dbis/anguillasearch/AnguillaSearch.java
+++ src/main/java/de/fernunihagen/dbis/anguillasearch/AnguillaSearch.java
@@ -7,9 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
@@ -18,9 +15,6 @@ import com.google.gson.JsonObject;
  * Main class of the AnguillaSearch project.
  */
 public final class AnguillaSearch {
-
-    //private static final Logger LOGGER = LoggerFactory.getLogger(AnguillaSearch.class);
-
     private static boolean color = false;
     
     private AnguillaSearch() {
@@ -37,27 +31,29 @@ public final class AnguillaSearch {
         //LOGGER.info("Starting AnguillaSearch...");
 
         /*
-         * Set the java.awt.headless property to true to prevent awt from opening windows.
-         * If the property is not set to true, the program will throw an exception when trying to 
-         * generate the graph visualizations in a headless environment.
+         * Set the java.awt.headless property to true to prevent awt from 
+         * opening windows.
+         * If the property is not set to true, the program will throw an 
+         * exception when trying to generate the graph visualizations in a 
+         * headless environment.
          */
         System.setProperty("java.awt.headless", "true");
-        //LOGGER.info("Java awt GraphicsEnvironment headless: {}", java.awt.GraphicsEnvironment.isHeadless());
         String[] seedURLs = null;
         String jsonPath = null;
         int rankMode = 2;
 
         Map<String, String> argMap = argsParser(args);
-        if(argMap.get("-r") != null) {
+        if (argMap.get("-r") != null) {
             rankMode = Integer.parseInt(argMap.get("-r"));
         }
         if (argMap.get("seedURLs") != null) {
             seedURLs = argMap.get("seedURLs").split(" ");
-        } else if(argMap.get("jsonPath") != null) {
+        } else if (argMap.get("jsonPath") != null) {
             jsonPath = argMap.get("jsonPath");
             try {
                 JsonObject json = Utils.parseJSONFile(jsonPath);
-                seedURLs = new Gson().fromJson(json.get("Seed-URLs"), String[].class);
+                seedURLs = new Gson().fromJson(json.get("Seed-URLs"), 
+                                      String[].class);
 
             } catch (IOException e) {
                 System.out.println(e.toString());
@@ -69,7 +65,8 @@ public final class AnguillaSearch {
             "http://buttery-redleicester.cheesy1",
             "http://rich-jarlsberg.cheesy1"};
             // print out seedURLs
-            System.out.println("No seedURLs or json provided using default seedURLs: " + String.join(", ", seedURLs));
+            System.out.println("No seedURLs or json provided using default"
+                               + " seedURLs: " + String.join(", ", seedURLs));
 
         }
         if (argMap.get("-color") != null) {
@@ -90,7 +87,8 @@ public final class AnguillaSearch {
                 System.out.println("Cosine Similarity.");
                 break;
             case 2:
-                System.out.println("Combination of Cosine Similarity and Pagerank.");
+                System.out.println("Combination of Cosine Similarity and"
+                                   + " Pagerank.");
                 break;
             default:
                 System.out.println("Unknown");
@@ -125,45 +123,44 @@ public final class AnguillaSearch {
      * @return a map of the command line arguments
      * @throws IllegalArgumentException
      */
-    public static Map<String, String> argsParser(final String[] args) throws IllegalArgumentException{
+    public static Map<String, String> argsParser(final String[] args) 
+                                      throws IllegalArgumentException {
         Map<String, String> argMap = new HashMap<>();
         int argCount = args.length;
-        // every argument support at maximum one parameter so we can use boolean.
+        // every argument support at maximum one parameter so we can use bool.
         Map<String, Boolean> validArgs = new HashMap<>();
         validArgs.put("-r", true);
         validArgs.put("-color", false);
 
-        if( argCount == 0) {
+        if (argCount == 0) {
             return argMap;
         }
         int i = 0;
         // process all arguments of type -arg and -arg parameter.
         while (i < argCount) {
-            if(validArgs.containsKey(args[i])) {
+            if (validArgs.containsKey(args[i])) {
                 String cArg = args[i];
-                if(validArgs.get(cArg)) {
-                    if(args[i+1].charAt(0) == '-') {
-                        invalidArguments(args[i+1]);
-                    }
-                    else {
-                        String cOpt = args[i+1];
+                if (validArgs.get(cArg)) {
+                    if (args[i + 1].charAt(0) == '-') {
+                        invalidArguments(args[i + 1]);
+                    } else {
+                        String cOpt = args[i + 1];
                         argMap.put(cArg, cOpt);
                         i++;
                     }
-                }
-                else {
+                } else {
                     argMap.put(cArg, "");
                 }
             // the args we are reading is not in our validArgs map, but might
             // out last argument, which is the target json or seed urls.
-            } else if (i == argCount -1) {
-                if(args[argCount -1].contains("http://") || 
-                args[argCount -1].contains("https://")) {
+            } else if (i == argCount - 1) {
+                if (args[argCount - 1].contains("http://") 
+                    || args[argCount - 1].contains("https://")) {
                     
-                    argMap.put("seedURLs", args[argCount -1]);
+                    argMap.put("seedURLs", args[argCount - 1]);
                 // here we assume that 
-                } else if(args[argCount -1].charAt(0) != '-') {
-                    argMap.put("jsonPath", args[argCount -1]);
+                } else if (args[argCount - 1].charAt(0) != '-') {
+                    argMap.put("jsonPath", args[argCount - 1]);
                 }
             } else {
                 invalidArguments(args[i]);
@@ -171,8 +168,8 @@ public final class AnguillaSearch {
             i++;
         }
         // check if the parameter for argument -r is in range.
-        if(argMap.containsKey("-r")) {
-            if(!argMap.get("-r").matches("^[012]$")) {
+        if (argMap.containsKey("-r")) {
+            if (!argMap.get("-r").matches("^[012]$")) {
                 System.out.println("-r option only accepts 0, 1 or 2 as" 
                                     + " parameter.");
                 System.out.println("0 - TF-IDF ranking.");
@@ -184,22 +181,26 @@ public final class AnguillaSearch {
         }
         return argMap;
     }
-    private static void printQuery(final Indexer index,final String query, final int rankMode) {
+    private static void printQuery(final Indexer index, final String query, 
+                                   final int rankMode) {
         List<SearchResult> searchResults = index.searchQuery(query, rankMode);
         List<String> queryLemmas = Parser.tokLem(query);
         int i = 1;
         if (searchResults.isEmpty()) {
-            System.out.format("No search results found for query \"%s\"%n%n", query);
+            System.out.format("No search results found for query \"%s\"%n%n",
+                              query);
         }
-        for(SearchResult searchResult : searchResults) {
+        for (SearchResult searchResult : searchResults) {
             System.out.format("Result %d:%n", i);
             System.out.format("URL: %s%n", searchResult.url());
             String title = searchResult.page().getTitle();
-            String sentenceWithQuery = getFirstQuerySentence(searchResult.page(), query);
+            String sentenceWithQuery = getFirstQuerySentence(searchResult.
+                                                            page(), query);
             String header = searchResult.page().getHeader();
             if (color) {
                 title = highlightString(title, queryLemmas);
-                sentenceWithQuery = highlightString(sentenceWithQuery, queryLemmas);
+                sentenceWithQuery = highlightString(sentenceWithQuery,
+                                                    queryLemmas);
                 header = highlightString(header, queryLemmas);
             }
             System.out.format("Title: \"%s\"%n", title);
@@ -209,7 +210,8 @@ public final class AnguillaSearch {
             i++;
         }
     }
-    private static String getFirstQuerySentence(Page p, String query) {
+    private static String getFirstQuerySentence(final Page p, 
+                                                final String query) {
         // we lemmatize the query to possibly get more matches
         List<String> queryLemmas = Parser.tokLem(query);
         // we split the content into sentences. The punction marks get removed.
@@ -229,20 +231,38 @@ public final class AnguillaSearch {
         if (firstSentence == null) {
             firstSentence = sentences[0];
         }
-        if(!matchedLemmaList.isEmpty() && color) {
+        if (!matchedLemmaList.isEmpty() && color) {
             firstSentence = highlightString(firstSentence, matchedLemmaList);
         }
         return firstSentence;
     }
-    private static String highlightString(String inputString, List<String> matchesToHighlight) {
+    private static String highlightString(final String inputString,
+                                    final List<String> matchesToHighlight) {
         String outString = inputString;
-        for(String match : matchesToHighlight) {
-            // (?i) to make the match case insenstive.
-            outString = outString.replaceAll( "(?i)" + match, "\u001B[32m" + match + "\u001B[0m");
+        List<String> allCaseMatchList = new ArrayList<>(matchesToHighlight);
+        /* We add lowercase or uppercase versions to matchesToHighlight so 
+         * don't change the case later on */
+        for (String match : matchesToHighlight) {
+            char firstC = match.charAt(0);
+            if (Character.isLowerCase(firstC)) {
+                String upperCaseV = Character.toUpperCase(firstC)
+                                    + match.substring(1);
+                allCaseMatchList.add(upperCaseV);
+            // we still check since not every character has a case and we do
+            // not want to duplicate entries.
+            } else if (Character.isUpperCase(firstC)) {
+                String lowerCaseV = Character.toLowerCase(firstC) 
+                                    + match.substring(1);
+                allCaseMatchList.add(lowerCaseV);
+            }
+        }
+        for (String match : allCaseMatchList) {
+            outString = outString.replaceAll(match, "\u001B[32m" + match 
+                                             + "\u001B[0m");
         }
         return outString;
     }
-    private static void invalidArguments(String unrecognized) {
+    private static void invalidArguments(final String unrecognized) {
         System.out.println("Unrecognized option: " + unrecognized);
         System.out.println("Try 'java -jar AnguillaSearch.java --help'");
         System.exit(1);
