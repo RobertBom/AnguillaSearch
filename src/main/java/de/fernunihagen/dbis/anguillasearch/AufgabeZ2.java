5ec69c5f866154d1ed568a626352f7e5a7f9d4d9
Robert Bombka <r.bombka@gmail.com>
2024-12-18 23:07:53 +0100

    Added class which creates graph with more info
    Class AufgabeZ2 creates a graph of the first 16 crawled pages of a net
    and displays the TF-IDF values at the vertices for a specific
    searchquery.

--- src/main/java/de/fernunihagen/dbis/anguillasearch/AufgabeZ2.java
+++ src/main/java/de/fernunihagen/dbis/anguillasearch/AufgabeZ2.java
@@ -20,7 +20,7 @@ import com.mxgraph.util.mxCellRenderer;
 import com.mxgraph.view.mxGraph;
 import com.mxgraph.view.mxStylesheet;
 
-public class AufgabeZ1 {
+public class AufgabeZ2 {
     static Map<String, Page> urlMap = new HashMap<>();
     
     public static void main(String[] args) {
@@ -43,6 +43,10 @@ public class AufgabeZ1 {
         index.printInfo();
         cleanLinks(crawledPages);
 
+        // execute search
+        String searchQuery = "flavor";
+        List<SearchResult> searchResults = index.searchQuery(searchQuery, 0);
+
         //generate urlMap
         for (Page p : crawledPages) {
             urlMap.put(p.getURL(), p);
@@ -50,14 +54,16 @@ public class AufgabeZ1 {
 
 		mxGraph graph = new mxGraph();
 		Object parent = graph.getDefaultParent();
+        // set HtmlLabels to true so we can use html formatting in labels.
+        graph.setHtmlLabels(true);
         Object[] vertices = new Object[16];
 
+        // set style for default cells
         mxStylesheet styleSheet = graph.getStylesheet();
         Hashtable<String, Object> style = new Hashtable<String, Object>();
         style.put("fillColor", "none");
         style.put("strokeColor", "black");
         style.put("fontColor", "black");
-        
         styleSheet.putCellStyle("default", style);
 
 		graph.getModel().beginUpdate();
@@ -69,11 +75,20 @@ public class AufgabeZ1 {
             // create a Vertex for every page
             for (int i = 0; i < crawledPages.size(); i++) {
                 String cUrl = crawledPages.get(i).getURL();
+                // get the ranking score current page, default is 0
+                double score = 0;
+                for (SearchResult sr : searchResults) {
+                    if (cUrl.equals(sr.url())) {
+                        score = sr.score();
+                        break;
+                    }
+                }
                 vertices[i] = graph.insertVertex(parent,
                                                  cUrl,
-                                                 cUrl,
-                                                 100,
-                                                 100,
+                                                 "<b>" + cUrl +"</b>" +
+                                                  "\n<font color=\"#404040\t\">TF-IDF: " + score + "</font>",
+                                                 20+40*i,
+                                                 20+20*i,
                                                  270,
                                                  30,
                                                  "default");
@@ -92,13 +107,49 @@ public class AufgabeZ1 {
             }
             var layout = new mxCircleLayout(graph);
             layout.execute(graph.getDefaultParent());
+
+
 		}
 		finally
 		{
 			graph.getModel().endUpdate();
 		}
+        // get height and width of our graph for placing our legend correctly
+        double graphHeight = graph.getGraphBounds().getHeight();
+        double graphWidth = graph.getGraphBounds().getWidth();
+        double legendWidth = 400;
+        double legendHeight = 150;
+        String legendHTML = "<table><tr><td>net:</td><td>" + jsonPath + 
+                            "</td></tr>" +
+                            "<tr><td>searchQuery: </td><td>" + searchQuery +
+                            "</td></tr></table>";
+        try
+		{
+            graph.insertVertex(parent,
+                                "Legend Headline",
+                                "<b>Legend</b>",
+                                graphWidth -500,
+                                graphHeight-200,
+                                legendWidth,
+                                20,
+                                "default;verticalAlign=top");
+
+            graph.insertVertex(parent,
+                                "Legend Content",
+                                legendHTML,
+                                graphWidth-500,
+                                graphHeight-180,
+                                legendWidth,
+                                100,
+                                "default;align=left;verticalAlign=top");
+        }
+		finally
+		{
+			graph.getModel().endUpdate();
+		}
+;
         BufferedImage image = mxCellRenderer.createBufferedImage(graph, null, 2, Color.WHITE, true, null);
-		File imgFile = new File("figures/net-graph.png");
+		File imgFile = new File("figures/" + searchQuery +  "-net-graph.png");
 		try { 
         	ImageIO.write(image, "PNG", imgFile);
 		} catch (IOException e) {
